fd88abca80f314c9450b8228fa1d53d742090c48
luis <madwinrar@gmail.com>
2022-07-22 01:23:29 -0400

    coool nuevo

--- src/assets/imgbox.js
+++ src/assets/imgbox.js
@@ -1,6 +1,6 @@
 import louck1 from './img/louck1.png';
 
+ /* eslint import/no-anonymous-default-export: [2, {"allowObject": true}] */
+export default { "img1": louck1,}
 
-export default {
-"img1": louck1,
-}
+
